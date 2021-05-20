5d09c32f516670e0a9fdc06572e7e8b6155c3426
Arpit <arpit0799.cse19@chitkara.edu.in>
2021-05-20 13:55:44 +0530

    Hello

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './MyComponents/main.css';
+
+
+import Header from './MyComponents/Header';
+import Footer from './MyComponents/Footer';
+import Main from './MyComponents/main';
+import About from './MyComponents/about';
+
+
+
+function App() {
+
+  return (
+    <>
+    
+     <About />
+     
+    
+    </>
+  );
+}
+
+export default App;
